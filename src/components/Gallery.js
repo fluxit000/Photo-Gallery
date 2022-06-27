f90e8bb33b84881d2846b99165e5a5ac27b912f7
Lukasz <lukasztrusk@gmail.com>
2022-06-27 19:54:27 +0200

    Add lazy loading

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,21 @@
 import {useContext} from "react"
 import './gallery.css'
 import galleryAPI from "../store/galleryAPI"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 const Gallery = props =>{
     const ctx = useContext(galleryAPI)
 
     return <div id="picture-container">
     {ctx.pictures.map(picture=>
-        <img className="picture" key={picture.id} src={picture.src.portrait}/>
+        <LazyLoadImage
+        className="picture" 
+        key={picture.id}
+        src={picture.src.portrait}
+        width={200}
+        height={300}
+        // placeholderSrc="loading.gif"
+        />
     )}
     </div>
 }
