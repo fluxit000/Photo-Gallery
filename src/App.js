193520966f1b122fae599abe2513dc65da62e521
Lukasz <lukasztrusk@gmail.com>
2022-06-28 16:34:15 +0200

    Start working on image popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Gallery from './components/Gallery';
 import SearchInput from './components/SearchInput'
+import Popup from './components/Popup';
 
 function App() {
 
 
   return (<main>
+    <Popup />
     <section>
       <SearchInput/>
     </section>
---
+++ src/components/Popup.js
@@ -0,0 +1,11 @@
+import './popup.css'
+
+const Popup = props =>{
+    return <div id='popup'>
+        <button className='right'></button>
+        <img src='https://images.pexels.com/photos/358457/pexels-photo-358457.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=1200&w=800'/>
+        <button className='left'></button>
+    </div>
+}
+
+export default Popup
